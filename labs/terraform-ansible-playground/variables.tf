9a4ea44b7e3de1e718347b4fb8cbe354d7ff1e44
DanielHemmati <danialups1997@gmail.com>
2026-07-19 10:30:14 +0200

    add ssh keys

--- labs/terraform-ansible-playground/variables.tf
+++ labs/terraform-ansible-playground/variables.tf
@@ -4,3 +4,8 @@ variable "ssh_public_key_path" {
   default     = "~/.ssh/terraform-ec2.pub"
 }
 
+variable "ssh_private_key_path" {
+  description = "Path to your SSH private key for Ansible"
+  type        = string
+  default     = "~/.ssh/terraform-ec2"
+}
